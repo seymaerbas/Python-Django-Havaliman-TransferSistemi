cba8b6a3b0b64d6cc7c9406bd5a588ed176ae65d
seymaerbas <66528209+seymaerbas@users.noreply.github.com>
2021-04-27 03:14:39 +0300

    Settings modeli oluşturuldu.Ayarlar girildi. Dinamik favicon eklendi

--- home/views.py
+++ home/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from home.models import Setting
+
+
 def index(request):
-    text="Merhaba django"
-    context = {'text': text}
+    setting = Setting.objects.get(pk=1)
+
+    context = {'setting': setting}
     return render(request, 'index.html', context)
