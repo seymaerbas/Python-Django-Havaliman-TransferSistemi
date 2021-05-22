7d5053ec8ec67c7d53e5beeb3ab035d2a5698a6e
seymaerbas <66528209+seymaerbas@users.noreply.github.com>
2021-05-22 22:19:16 +0300

    Otomatik slug oluşturuldu

--- transfer/admin.py
+++ transfer/admin.py
@@ -19,9 +19,10 @@ class CategoryAdmin(admin.ModelAdmin):
 class TransferAdmin(admin.ModelAdmin):
 
     list_display = ['title', 'category', 'base_price', 'km_price', 'capasity', 'image_tag', 'status']
-    readonly_fields = ('image_tag',)
+    readonly_fields = ('image_tag', )
     list_filter = ['status', 'category']
     inlines = [TransferImageInline]
+    prepopulated_fields = {'slug': ('title',)}
 
 class ImagesAdmin(admin.ModelAdmin):
     list_display = ['title', 'transfer', 'image_tag']
@@ -32,6 +33,7 @@ class CategoryAdmin2(DraggableMPTTAdmin):
     list_display = ('tree_actions', 'indented_title',
                     'related_transfers_count', 'related_transfers_cumulative_count')
     list_display_links = ('indented_title',)
+    prepopulated_fields = {'slug': ('title',)}
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
--- transfer/models.py
+++ transfer/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 from django.forms import ModelForm
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
 from mptt.fields import TreeForeignKey
@@ -19,7 +20,7 @@ class Category(MPTTModel):
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null=False, unique=True)
     parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -34,6 +35,9 @@ class Category(MPTTModel):
             full_path.append(k.title)
             k = k.parent
         return ' >> '.join(full_path[::-1])
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
 
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
@@ -54,7 +58,7 @@ class Transfer(models.Model):
     capasity = models.FloatField()
     detail = RichTextUploadingField()
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField(blank=True)
+    slug = models.SlugField(null=False, unique=True)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -62,10 +66,14 @@ class Transfer(models.Model):
     def __str__(self):
         return self.title
 
+
+
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
 
+    def get_absolute_url(self):
+        return reverse('transfer_detail', kwargs={'slug': self.slug})
 
 class Images(models.Model):
     transfer = models.ForeignKey(Transfer, on_delete=models.CASCADE)
