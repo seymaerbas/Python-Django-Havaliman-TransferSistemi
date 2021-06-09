08af922a6627cec7eef13219928b2fad030e9df8
seymaerbas <66528209+seymaerbas@users.noreply.github.com>
2021-06-09 18:01:41 +0300

    Hatalar giderildi

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('update/', views.user_update, name='user_update'),
+    path('password/', views.change_password, name='change_password'),
+    path('comments/', views.comments, name='comments'),
+    path('deletecomment/<int:id>', views.deletecomment, name='deletecomment'),
     #path('<int:question_id>/', views.detail, name='detail'),
 ]
